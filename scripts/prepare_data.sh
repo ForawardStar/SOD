babfff9282525cf680cff70bccca9c3636018335
ForawardStar <32625467+ForawardStar@users.noreply.github.com>
2025-03-30 09:28:43 +0800

    Delete scripts/prepare_data.sh

--- scripts/prepare_data.sh
+++
@@ -1,5 +0,0 @@
-set -o xtrace
-
-# download data from github address
-wget -c --no-check-certificate --content-disposition https://github.com/yuhuan-wu/EDN/releases/download/v1.0/SOD_datasets.zip -O SOD_datasets.zip
-unzip -n SOD_datasets.zip -d data/
